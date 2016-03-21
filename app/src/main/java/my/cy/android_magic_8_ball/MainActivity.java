fb51cc9882d46d4e82994a6928fca26abcd517a8
CY Lim <cylim226@icloud.com>
2016-03-21 19:23:29 +1100

    Initial commit

---
+++ app/src/main/java/my/cy/android_magic_8_ball/MainActivity.java
@@ -0,0 +1,13 @@
+package my.cy.android_magic_8_ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
